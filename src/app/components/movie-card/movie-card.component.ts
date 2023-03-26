6fe0f9d0f78c4f9faadca35ea1b4316ae603ca7f
Nicolas Marqui <nick.marqui@gmail.com>
2023-03-26 17:40:08 +0100

    Added favorites func

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,3 +1,7 @@
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+import { ActivatedRoute } from '@angular/router';
+import { LocalService } from './../../_services/local.service';
 import { IPopularMovies } from './../../_models/index';
 import { Component, Input } from '@angular/core';
 import { faHeart, faCheck, faFilm } from '@fortawesome/free-solid-svg-icons';
@@ -16,8 +20,88 @@ export class MovieCardComponent {
 
   poster: string = '';
   poster_url: string = `https://image.tmdb.org/t/p/w500`;
+  isFavorited: boolean = false;
+  hasRouteId = this.route.snapshot.paramMap.get('id');
+  shouldToggleAnimation: boolean = false;
+
+  options: AnimationOptions = {
+    path: '/assets/animations/favorite.json',
+  };
+
+  constructor(private local: LocalService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.poster = `${this.poster_url}/${this.movie?.poster_path}`;
+    this.checkIfIsBeingFavorited();
   }
+
+  toggleFavorites() {
+    if (this.isFavorited) {
+      return this.removeFromFavorite();
+    }
+
+    const objToSave = {
+      id: this.movie.id,
+      poster: this.movie.poster_path,
+      title: this.movie.title,
+      favorited: true,
+      // watched: this.checkIfFavHasBeenWatched(),
+      favoritedDate: new Date(),
+    };
+
+    const all = JSON.parse(this.local.getData('favorites') || '[]');
+    const save = [...all, objToSave];
+
+    this.local.saveData('favorites', JSON.stringify(save));
+    this.showAnimation();
+    this.checkIfIsBeingFavorited();
+  }
+
+  checkIfIsBeingFavorited() {
+    const allFavoritedMovies = this.local.getData('favorites');
+    const movieId = this.hasRouteId ? Number(this.hasRouteId) : this.movie.id;
+
+    if (!allFavoritedMovies) return;
+
+    const parsedFavorited = JSON.parse(allFavoritedMovies);
+
+    if (parsedFavorited) {
+      const isTracked = parsedFavorited.filter(
+        (favorited: any) => favorited.id === movieId
+      );
+
+      if (isTracked && isTracked.length > 0) {
+        this.isFavorited = true;
+      }
+    }
+  }
+
+  removeFromFavorite() {
+    const all = JSON.parse(this.local.getData('favorites') || '[]');
+    const movieId = this.hasRouteId ? Number(this.hasRouteId) : this.movie.id;
+
+    if (all && all.length > 0) {
+      let alter = function (movie: any) {
+        return movie.id !== movieId;
+      };
+
+      const save = all.filter(alter);
+
+      this.local.saveData('favorites', JSON.stringify(save));
+      this.isFavorited = false;
+      this.checkIfIsBeingFavorited();
+    }
+  }
+
+  showAnimation() {
+    this.shouldToggleAnimation = true;
+
+    window.setTimeout(() => {
+      this.shouldToggleAnimation = false;
+    }, 1200);
+  }
+
+  checkIfFavHasBeenWatched() {}
+
+  animationCreated(animationItem: AnimationItem): void {}
 }
